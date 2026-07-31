01483b3ce4657a8888c36f170418c80b84dcfabe
anas <anasgulzarco@gmail.com>
2026-07-31 16:45:23 +0530

    Add files via upload

---
+++ conditional statement/else_if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+int age;
+printf("enter your age :");
+scanf("%d",&age);
+if(age>=18){
+    printf("adult");
+}
+else if (age>=13 && age<18) {
+    printf("teenager");
+}
+else{
+    printf("child");
+}
+return 0;
+}
